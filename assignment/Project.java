d8b7a80be8d616ea9aa9797224efaf90523f44dd
ahujatanisha <tanisha18csu215@ncuindia.edu>
2019-01-28 18:46:08 +0530

    added

---
+++ assignment/Project.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+class Project
+{
+
+   String name;
+   String role;
+   String responsibilities;
+
+   Project(String name, String role )
+   {
+      this.name=name;
+      this.role=role;
+    }
+
+   public void setName(String name) 
+   {   
+      this.name=name;
+   }
+
+  public String getName()
+  
+  {
+    return name;
+  }
+
+  public void setRole(String role) 
+  {   
+    this.role=role;
+  }
+
+  public String getRole()
+
+  {
+    return role;
+  }
+
+  public void setResp(String responsibilities) 
+  {   
+
+    this.responsibilities=responsibilities;
+  }
+
+  public String get()
+
+  {
+    return responsibilities;
+  }
+  public void displayProject()
+  {
+
+    System.out.println("project name of the student \n"+ "name =" + name +"Role =" + role );
+
+  }
+}
+
+
+
