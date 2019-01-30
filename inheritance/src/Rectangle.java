b778ca780b64289e87903e39c013b03c8ab77d7f
ahujatanisha <tanisha18csu215@ncuindia.edu>
2019-01-31 01:29:42 +0530

    added

---
+++ inheritance/src/Rectangle.java
@@ -0,0 +1,38 @@
+class Rectangle
+{
+	int l,b;
+	void display()
+	{
+	System.out.println((l*b));
+    }
+	Rectangle (int x ,int y)
+	{
+	this.l=x;
+	this.b=y;
+
+	}
+}
+
+class Box extends Rectangle
+{
+    int h ;
+    void display()
+    {
+    int a=l*b*h;
+    System.out.println(a);
+    }
+    Box(int x, int y,int z)
+    {
+    super(x,y);
+    this.h=z;
+    }
+}
+class Test
+{ 
+    public static void main(String args[])
+    {
+    Box ob=new Box(1,1,2);
+    ob.display();
+    }
+}
+
